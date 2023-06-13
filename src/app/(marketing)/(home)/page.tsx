edfdeb59b9e5ce1281249f26fe5959907f70811d
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-06-13 15:44:27 +0200

    chore: add SEO metadata to HTML head

--- src/app/(marketing)/(home)/page.tsx
+++ src/app/(marketing)/(home)/page.tsx
@@ -1,5 +1,9 @@
 import { Hero } from './Hero';
 
+export const metadata = {
+  title: 'Next.js Blueprint | Home',
+};
+
 export default function Home() {
   return (
     <div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import './globals.css';
 import { PropsWithChildren } from 'react';
 
+export const metadata = {
+  authors: { name: 'Imraan-Hendricks', url: 'https://www.imraanhendricks.com' },
+  title: 'Next.js Blueprint',
+  description:
+    'Next.js Blueprint is a foundational project template for rapidly developing web applications using Next.js and TypeScript.',
+};
+
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='en'>
